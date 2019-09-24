20ead53604c3e8c022012e250300df762df2d2f9
Ganvs <gustavo.anverze@gmail.com>
2019-09-24 14:27:18 -0300

    main component changed to accept state

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,27 +1,42 @@
-import React from 'react';
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
 
 import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
 import { Container, Form, SubmitButton } from './styles';
 
-export default function Main() {
-  return (
-    <Container>
-      <h1>
-        <FaGithubAlt />
-        Repositórios
-      </h1>
+export default class Main extends Component {
+  state = {
+    newRepo: '',
+  };
 
-      <Form onSubmit={() => {}}>
-        <input
-          type="text"
-          placeholder="Adicionar repositório (usuário/repositório)"
-        />
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
 
-        <SubmitButton disabled>
-          <FaPlus color="#fff" size={14} />
-        </SubmitButton>
-      </Form>
-    </Container>
-  );
+  render() {
+    const { newRepo } = this.state;
+
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositórios
+        </h1>
+
+        <Form onSubmit={() => {}}>
+          <input
+            type="text"
+            placeholder="Adicionar repositório (usuário/repositório)"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+
+          <SubmitButton disabled>
+            <FaPlus color="#fff" size={14} />
+          </SubmitButton>
+        </Form>
+      </Container>
+    );
+  }
 }
