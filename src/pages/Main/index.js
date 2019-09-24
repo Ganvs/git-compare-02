89e47ee8445b66cc300bc197ce2b6c8b3a2bc739
Ganvs <gustavo.anverze@gmail.com>
2019-09-24 12:09:33 -0300

    first component (form to add repositoies)

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
-  return <Title>Main</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input
+          type="text"
+          placeholder="Adicionar repositório (usuário/repositório)"
+        />
+
+        <SubmitButton disabled>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
