10a0f41263fa081b8b5ac457375020216838f5b1
Ganvs <gustavo.anverze@gmail.com>
2019-09-25 10:07:00 -0300

    saving data in localStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -13,6 +13,24 @@ export default class Main extends Component {
     loading: false,
   };
 
+  // Carregar os dados do localStorage ('banco de dados' do navegador)
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // Salvar os dados no localStorage ('banco de dados' do navegador)
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   };
