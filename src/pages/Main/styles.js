03f567e1b3dd8ec13fa9074984a9129f41046b6c
Ganvs <gustavo.anverze@gmail.com>
2019-09-25 11:48:48 -0300

    component refactor and repository page style

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,30 +1,5 @@
 import styled, { keyframes, css } from 'styled-components';
 
-export const Container = styled.div`
-  max-width: 700px;
-  background: #fff;
-  border-radius: 4px;
-  box-shadow: 0 0 20px rgba(0, 0, 0, 0.4);
-  padding: 30px;
-  margin: 80px auto;
-  transition: all 0.2s ease-out;
-
-  h1 {
-    font-size: 20px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-
-    svg {
-      margin-right: 10px;
-    }
-  }
-
-  &:hover {
-    transform: scale(1.01);
-  }
-`;
-
 export const Form = styled.form`
   margin-top: 30px;
   display: flex;
