64318aeb8932ceeec095839249a1559aef060e2b
Ganvs <gustavo.anverze@gmail.com>
2019-09-25 14:29:37 -0300

    added issue labels

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -69,6 +69,9 @@ export default class Repository extends Component {
                 <strong>
                   <a href={issue.html_url}>{issue.title}</a>
                   {/** LABELS * */}
+                  {issue.labels.map(label => (
+                    <span key={String(label.id)}>{label.name}</span>
+                  ))}
                 </strong>
                 <p>{issue.user.login}</p>
               </div>
